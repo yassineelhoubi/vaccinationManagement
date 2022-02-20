4001679a716027fdf498a77d0103c53cd5540ab2
yassineelhoubi <elhoubiyassine@gmail.com>
2022-02-20 21:14:58 +0100

    manage header button content

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -12,7 +12,8 @@ const Header: React.FC<headerProps> = ({setTakeShot , takeShot}) => {
         Morocco Vaccination
       </a>
       {/* button */}
-      <a href="#" onClick={() => { setTakeShot(!takeShot) }} className="bg-indigo-500 hover:bg-indigo-600 active:bg-indigo-700 focus-visible:ring ring-indigo-300 text-white text-sm md:text-base font-semibold text-center rounded-lg outline-none transition duration-100 px-8 py-3">Take your vaccine</a>
+      <a href="#" onClick={() => { setTakeShot(!takeShot) }} className="bg-indigo-500 hover:bg-indigo-600 active:bg-indigo-700 focus-visible:ring ring-indigo-300 text-white text-sm md:text-base font-semibold text-center rounded-lg outline-none transition duration-100 px-8 py-3">
+        {!takeShot?"Take your vaccine":"Back to Home"}</a>
     </header>
   )
 }
