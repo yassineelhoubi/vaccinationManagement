0cbadb47bccc88969c050668d212fb87716fdc90
yassineelhoubi <elhoubiyassine@gmail.com>
2022-02-22 17:02:24 +0100

    Add component to validate user info and change stepper numbering

--- src/components/index.ts
+++ src/components/index.ts
@@ -7,3 +7,4 @@ export { ChoiceShot } from './ChoiceShot';
 export { DiseaseOrTreatments } from './DiseaseOrTreatments';
 export { SideEffects } from './SideEffects';
 export { UserFormCIN } from './UserFormCIN';
+export { UserInfoValidation } from './UserInfoValidation';
