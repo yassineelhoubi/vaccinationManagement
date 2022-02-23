7d10aa8ba6ae8c259d1f3152468d4179e5eecf61
yassineelhoubi <elhoubiyassine@gmail.com>
2022-02-23 11:36:29 +0100

    improve responsive

--- src/components/UserInfoValidation.tsx
+++ src/components/UserInfoValidation.tsx
@@ -1,28 +1,30 @@
 import FormControl from '@mui/material/FormControl';
 import Box from '@mui/material/Box';
-import { FormLabel, TextField } from '@mui/material';
+import { Container, FormLabel, TextField } from '@mui/material';
 
 const UserInfoValidation: React.FC = () => {
     return (
-        <Box >
-            <FormControl sx={{
-                '& .MuiTextField-root': { m: 1, width: '25ch' },
-            }}>
-                <div className="text-center"><FormLabel className="text-wider mb-3">Validate Your Information </FormLabel></div>
-                <div>
-                    <TextField id="outlined-basic" margin="normal" label="First Name" variant="outlined" />
-                    <TextField id="outlined-basic" margin="normal" label="Last Name" variant="outlined" />
-                </div>
-                <div>
-                    <TextField id="outlined-basic" margin="normal" label="Phone" variant="outlined" />
-                    <TextField id="outlined-basic" margin="normal" label="Email" variant="outlined" />
-                </div>
-                <div>
-                    <TextField id="outlined-basic" margin="normal" label="City" variant="outlined" />
-                    <TextField id="outlined-basic" margin="normal" label="Address" variant="outlined" />
-                </div>
-            </FormControl>
-        </Box>
+
+        <FormControl sx={{
+            mx: "auto",
+            '& .MuiTextField-root': { m: 1, width: '100%' },
+        }}>
+            <FormLabel className="text-wider mb-3 lg:flex lg:justify-center hidden">Validate Your Information </FormLabel>
+            <div className="flex w-full justify-center items-center lg:flex-row flex-col">
+                <TextField size="small" id="outlined-basic" margin="normal" label="First Name" variant="outlined" />
+                <TextField size="small" id="outlined-basic" margin="normal" label="Last Name" variant="outlined" />
+            </div>
+            <div className="flex w-full justify-center items-center lg:flex-row flex-col">
+                <TextField size="small" id="outlined-basic" margin="normal" label="Phone" variant="outlined" />
+                <TextField size="small" id="outlined-basic" margin="normal" label="Email" variant="outlined" />
+            </div>
+            <div className="flex w-full justify-center items-center lg:flex-row flex-col">
+                <TextField size="small" id="outlined-basic" margin="normal" label="City" variant="outlined" />
+                <TextField size="small" id="outlined-basic" margin="normal" label="Address" variant="outlined" />
+            </div>
+
+        </FormControl>
+
     )
 }
 
